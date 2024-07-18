f7f6c11cdc8b19741cdfd39f9787f1b6e705040e
It's Parsa <90866768+Parsabz@users.noreply.github.com>
2024-07-18 17:59:06 +0400

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,1 +1,31 @@
 // here's would be my notebook for MakingNote/Practicing concepts, you should make one as well if you wanna rock and roll.
+
+
+//if-else
+
+var age = 70;
+// if (age<20) {
+console. log( 'Teenager!');
+// } else if (age >= 20 && age ‹ 70) { console. log('A young man!');
+7 // } else {
+console. log('Not so young anymore!');
+if (age > 20) {
+} else {
+console. log('Not a teen anymore!');
+console. log( 'Teen!');
+age › 20 ? console.log('Not a teen anymore!') : console. log( 'Teen!');
+condition ? vall : val2
+
+
+//Objects
+
+var person = new Object();
+person.name = "James" ;
+person.age = 43;
+person.height = 1.76;
+person.married = true;
+person["name"] = "Kirk"
+person.car = (}:
+person.car.make = "Honda";
+
+
