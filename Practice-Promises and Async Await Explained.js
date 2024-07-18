d6e482451264e56b111353baba8b3bd88ad035a8
It's Parsa <90866768+Parsabz@users.noreply.github.com>
2024-07-19 01:56:32 +0400

    Update Practice-Promises and Async Await Explained.js

--- Practice-Promises and Async Await Explained.js
+++ Practice-Promises and Async Await Explained.js
@@ -32,3 +32,16 @@ console. log (data);
     } 
 };
 fetchData();
+                //finally
+const axios = require("axios");
+const fetchData = async () => {
+    try {
+const data = await axios.get("https://cat-fact.herokuapp.com/facts")
+console. log (data);
+} catch (err) {
+    console. log (err);
+    } finally {
+        console.log("shit");
+    }
+};
+fetchData();
