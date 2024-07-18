c8f4957cb020cefab9004bb18a8c056291604672
It's Parsa <90866768+Parsabz@users.noreply.github.com>
2024-07-19 01:53:46 +0400

    Update Practice-Promises and Async Await Explained.js

--- Practice-Promises and Async Await Explained.js
+++ Practice-Promises and Async Await Explained.js
@@ -11,3 +11,24 @@ console.log (res);
         console.log(err); 
     });
 console.log(data);
+
+
+//or you can use Async/Await
+const axios = require("axios");
+const fetchData = async () => {
+const data = await axios.get("https://cat-fact.herokuapp.com/facts")
+console. log (data);
+}
+fetchData();
+
+          //use try/catch for errors when using Async/Await
+const axios = require("axios");
+const fetchData = async () => {
+    try {
+const data = await axios.get("https://cat-fact.herokuapp.com/fact1s")
+console. log (data);
+} catch (err) {
+    console. log (err);
+    } 
+};
+fetchData();
