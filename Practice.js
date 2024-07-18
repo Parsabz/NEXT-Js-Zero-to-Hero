335c814e1b007b7f44bba09b08fa3fc2ddc6ba64
It's Parsa <90866768+Parsabz@users.noreply.github.com>
2024-07-18 17:52:11 +0400

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,1 +1,1 @@
-here's would be my notebook for MakingNote/Practicing concepts, you should make one as well if you wanna rock and roll.
+// here's would be my notebook for MakingNote/Practicing concepts, you should make one as well if you wanna rock and roll.
