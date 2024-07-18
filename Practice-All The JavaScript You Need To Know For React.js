16123602d03372979029204addf0cd168d577689
It's Parsa <90866768+Parsabz@users.noreply.github.com>
2024-07-18 21:19:03 +0400

    Create Practice-All The JavaScript You Need To Know For React.js

---
+++ Practice-All The JavaScript You Need To Know For React.js
@@ -0,0 +1,18 @@
+// notes made from this tutorial https://www.youtube.com/watch?v=m55PTVUrlnA&t=300s
+
+// Arrow Functions
+
+function DoSomething() { //normal function
+｝
+const DoSomething = () => { //Arrow Function
+}
+
+// Annonymous Functions (React thing-A function without a name)
+
+‹button
+onclick=｛G） ＝> {
+console. log ("hello world");
+}}
+></button>;
+
+
